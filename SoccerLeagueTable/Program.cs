e8114d4440d8bfa3eea6cd15033d7d7eeaf13188
Courtland Hacker <Courtland9777@gmail.com>
2022-09-13 00:09:53 -0400

    Add project files.

---
+++ SoccerLeagueTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+const string file = @"football.dat";
+if (File.Exists(file))
+{
+    var teamName = string.Empty;
+    var minSpan = 1000;
+    var footballData = File.ReadLines(file, Encoding.UTF8);
+    
+    foreach (var line in footballData.Skip(1))
+    {
+        if(line.Contains("---")) continue;
+        
+        var lineArr = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var tempSpan = int.Parse(lineArr[6][..2]) - int.Parse(lineArr[8][..2]);
+        if (tempSpan < minSpan)
+        {
+            minSpan = tempSpan;
+            teamName = lineArr[1];
+        }
+    }
+    Console.WriteLine($"The team with the minimum span is {teamName}");
+}
+else
+{
+    Console.WriteLine("{file} doesn't exist.");
+}
